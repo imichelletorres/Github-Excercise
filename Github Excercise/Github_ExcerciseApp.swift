80c86f33ac341bc9751a72b10fbbdf8f6b26cd27
Michelle Torres <imichelletorres@icloud.com>
2023-06-10 14:16:57 -0700

    Initial Commit

---
+++ Github Excercise/Github_ExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExcerciseApp.swift
+//  Github Excercise
+//
+//  Created by Michelle Torres on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
